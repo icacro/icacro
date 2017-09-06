d3721ebe58949d07bd4b30610be6de6684662c97
Per Flitig <per.flitig@ica.se>
2017-09-06 16:38:36 +0200

    Lagt till rätt urlar till CTAs samt mindre fixar

--- erbjudande/index.js
+++ erbjudande/index.js
@@ -3,6 +3,8 @@
 
 	var styles = '<style type="text/css">' +
 		'@media only screen and (min-width: 980px) { .cro .hero-with-main .main { margin-top: -35rem; } }' +
+		'@media only screen and (min-width: 700px) and (max-width: 979px) { .cro .hero-with-main .main { margin-top: -25rem; } }' +
+		'@media only screen and (max-width: 699px) { .cro .hero-with-main .main { margin-top: auto; } }' +
 		'.cro .bookning-container { background: none; }' +
 		'.cro .bookning-container::before { display: none; }' +
 		'.cro .category-bookning-section p { font-size: 1.4rem; }' + 
@@ -10,7 +12,7 @@
 		'.cro .news-module-container .item-module { width: auto; }' +
 		'.cro .ica-card-module { float: none; }' +
 		'.cro .ica-card-module .heading-icatext { margin-bottom: 5px; }' +
-		'.cro .ica-card-module a { color: #A02971; }' +
+		'.cro .ica-card-module a { color: #A02971; text-decoration: none; }' +
 		'.cro .ica-card-module p { line-height: 1.3; margin-bottom: .5em; }' +
 		'.cro .skaffa-ica-kort { position: absolute; z-index: 1; background-color: #D1C1D9; width: 100%; }' +
 		'.cro .skaffa-ica-kort > div { max-width: 1220px; margin: 0 auto; padding: 0 20px; }' +
@@ -18,18 +20,19 @@
 		'.cro .skaffa-ica-kort p { display: inline-block; margin: 10px 0 0 10px; font-size: 1.4rem; font-weight: bold; }' + 
 		'.cro .skaffa-ica-kort .button { float: right; margin: 6px 0 0 0; }' + 
 		'</style>';
-	var ctaTarget = 'https://';
+	var ctaTarget = 'https://www.ica.se/logga-in/?returnurl=https%3a%2f%2fwww.ica.se%2fErbjudanden%2fNojeserbjudanden%2fVing%2f';
 	var ctaHtml = '<div class="column size20of20 lg_size12of20 full-bleed-background pl" style="background-color: white;">' +
 		'<h1>Spara alltid minst 600:- per resa</h1>' +
 		'<p>' + $('.article-name').find('p').text() + '</p>' +
 		'<a href="' + ctaTarget + '" class="button button--auto-width">Klicka här för resor och rabatter</a>' +
 		'<div class="ruler"></div>' +
 		'</div>';
+	var getCardTarget = 'https://www.ica.se/ansokan/?returnurl=https%3a%2f%2fwww.ica.se%2fErbjudanden%2fNojeserbjudanden%2fVing%2f'
 	var getCardHtml = '<div class="skaffa-ica-kort pl grid_fluid">' +
     	'<div class="column size20of20">' +
     	'<img src="/Templates/CardBank/Views/images/ica-card-big.png">' +
     	'<p>Skaffa ICA-kort och ta del av alla erbjudanden</p>' +
-    	'<a href="#" class="button button--small">Få rabatt</a>' +
+    	'<a href="' + getCardTarget +'" class="button button--small">Få rabatt</a>' +
 		'</div>' +
 		'</div>'
 
