c52cd41ae6c86bfe3a05e3bc67458227dff13fad
Per Flitig <per.flitig@ica.se>
2017-09-06 15:30:13 +0200

    Lagt till övergripande klass för större specificitet samt finlir kring skaffa kort bannern

--- erbjudande/index.js
+++ erbjudande/index.js
@@ -2,19 +2,21 @@
 	'use strict';
 
 	var styles = '<style type="text/css">' +
-		'@media only screen and (min-width: 980px) { .hero-with-main .main { margin-top: -35rem; } }' +
-		'.bookning-container { background: none; }' +
-		'.bookning-container::before { display: none; }' +
-		'.category-bookning-section .ruler { margin-top: 3rem; border-bottom: 1px solid #d9d6d2; }' + 
-		'.category-bookning-section p { font-size: 1.4rem; }' + 
-		'.news-module-container .item-module { width: auto; }' +
-		'.ica-card-module { float: none; }' +
-		'.ica-card-module .heading-icatext { margin-bottom: 5px; }' +
-		'.ica-card-module a { color: #A02971; }' +
-		'.ica-card-module p { line-height: 1.3; margin-bottom: .5em; }' +
-		'.ica-kort-icon { height: 4rem; display: inline-block; }' +
-		'.skaffa-ica-kort { position: absolute; z-index: 1; background-color: #D1C1D9; /* width: 100%; */ display: flex; flex-wrap: wrap; }' +
-		'.skaffa-ica-kort { display: inline-block; font-weight: bold; }' + 
+		'@media only screen and (min-width: 980px) { .cro .hero-with-main .main { margin-top: -35rem; } }' +
+		'.cro .bookning-container { background: none; }' +
+		'.cro .bookning-container::before { display: none; }' +
+		'.cro .category-bookning-section p { font-size: 1.4rem; }' + 
+		'.cro .category-bookning-section .ruler { margin-top: 3rem; border-bottom: 1px solid #d9d6d2; }' + 
+		'.cro .news-module-container .item-module { width: auto; }' +
+		'.cro .ica-card-module { float: none; }' +
+		'.cro .ica-card-module .heading-icatext { margin-bottom: 5px; }' +
+		'.cro .ica-card-module a { color: #A02971; }' +
+		'.cro .ica-card-module p { line-height: 1.3; margin-bottom: .5em; }' +
+		'.cro .skaffa-ica-kort { position: absolute; z-index: 1; background-color: #D1C1D9; width: 100%; }' +
+		'.cro .skaffa-ica-kort > div { max-width: 1220px; margin: 0 auto; padding: 0 20px; }' +
+		'.cro .skaffa-ica-kort img { float: left; height: 4rem; display: inline-block; }' +
+		'.cro .skaffa-ica-kort p { display: inline-block; margin: 10px 0 0 10px; font-size: 1.4rem; font-weight: bold; }' + 
+		'.cro .skaffa-ica-kort .button { float: right; margin: 6px 0 0 0; }' + 
 		'</style>';
 	var ctaTarget = 'https://';
 	var ctaHtml = '<div class="column size20of20 lg_size12of20 full-bleed-background pl" style="background-color: white;">' +
@@ -23,9 +25,9 @@
 		'<a href="' + ctaTarget + '" class="button button--auto-width">Klicka här för resor och rabatter</a>' +
 		'<div class="ruler"></div>' +
 		'</div>';
-	var getCardHtml = '<div class="skaffa-ica-kort pl grid-fluid">' +
+	var getCardHtml = '<div class="skaffa-ica-kort pl grid_fluid">' +
     	'<div class="column size20of20">' +
-    	'<img src="/Templates/CardBank/Views/images/ica-card-big.png" class="ica-kort-icon">' +
+    	'<img src="/Templates/CardBank/Views/images/ica-card-big.png">' +
     	'<p>Skaffa ICA-kort och ta del av alla erbjudanden</p>' +
     	'<a href="#" class="button button--small">Få rabatt</a>' +
 		'</div>' +
@@ -35,6 +37,7 @@
 	var side = $('.category-bookning-section .column:nth-child(2)');
 	var saknarInloggning = $('.news-module-container .ica-card-module');
 	var villkor = $('.text-module', $('.composition-modules .column:nth-child(1)'));
+	var hero = $('.top-hero-header');
 
 
 	addStyles();
@@ -45,13 +48,16 @@
 
 
 	function manipulateDom() {
+		$('body').addClass('cro');
+
+		hero.prepend(getCardHtml);
+
 		side.html('');
 		side.addClass('news-module-container');
 
 		var wrapper = $('<div class="item-module"></div>');
 		wrapper.append(saknarInloggning);
 		side.append(wrapper);
-		// side.append(saknarInloggning);
 
 		$('.category-bookning-section').prepend(ctaHtml);
 
