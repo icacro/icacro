c2e79a4c436d694d2923c866378ed03b460f8185
Per Flitig <per.flitig@ica.se>
2017-11-20 17:02:26 +0100

    Lagt till lintregler och städat bort några fel i templaten

--- src/template.js
+++ src/template.js
@@ -9,35 +9,36 @@
 
 // import { ICACRO, $ELM } from '../../icacro/src/main';
 
-(function($) {
-    'use strict';
-    hj;
-    const helperVersion = '#version';
-    const test = {
-        addStyles() {
-          const styles = `
-            .cro { }
-          `;
-          return styles;
-        },
-        manipulateDom() {}
+(function ($) {
+  'use strict';
+
+  hj;
+  const helperVersion = '#version';
+  const test = {
+    addStyles() {
+      const styles = `
+      .cro { }
+      `;
+      return styles;
+    },
+    manipulateDom() {}
+  };
+
+  const loadJS = (callback) => {
+    const script = document.createElement('script');
+    script.setAttribute('async', '')
+    script.setAttribute('src', `https://cdn.rawgit.com/Banzaci/icacro/v${helperVersion}/dist/main.min.js`);
+    document.querySelector('head').appendChild(script);
+    script.onreadystatechange = script.onload = () => {
+      callback();
     };
+  }
 
-    const loadJS = (callback) => {
-      const script = document.createElement('script');
-      script.setAttribute('async', '')
-      script.setAttribute('src', `https://cdn.rawgit.com/Banzaci/icacro/v${helperVersion}/dist/main.min.js`);
-      document.querySelector('head').appendChild(script);
-      script.onreadystatechange = script.onload = () => {
-        callback();
-      };
-    }
-
-    $(document).ready(function (){
-      loadJS(() => {
-        Object.assign(test, ICACRO());
-        test.style(test.addStyles());
-        test.manipulateDom();
-      });
+  $(document).ready(() => {
+    loadJS(() => {
+      Object.assign(test, ICACRO());
+      test.style(test.addStyles());
+      test.manipulateDom();
     });
+  });
 })(jQuery);
