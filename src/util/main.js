e42fea1963290e9450cd2dc844c734e5a4923188
Michael Larsson <michael.larsson@ica.se>
2017-12-07 16:32:33 +0100

    Added hide function

--- src/util/main.js
+++ src/util/main.js
@@ -66,6 +66,13 @@ function $ELM_ELEMENT(element) {
       }
       throw new Error(`${url} Element does not exist! Function 'href'`);
     },
+    removeClass(className) {
+      if (element) {
+        element.classList.remove(className);
+        return this;
+      }
+      throw new Error(`${className} Element does not exist! Function 'removeClass'`);
+    },
     appendFirst(child) {
       const c = child.nodeType ? child : child.element;
       if (element) {
@@ -109,7 +116,8 @@ function $ELM_ELEMENT(element) {
     },
     children(arg) {
       if (arg) {
-        const list = Array.from(element.getElementsByTagName(arg));
+        const func = reservedElements.some(elm => elm === arg) ? ['getElementsByTagName'] : ['querySelectorAll'];
+        const list = Array.from(element[func](arg));
         return list.map(child => new $ELM_ELEMENT(child));
       }
       const list = Array.from(element.childNodes);
@@ -124,14 +132,21 @@ function $ELM_ELEMENT(element) {
     },
     insertAfter(targetName) {
       if (element) {
-        const target = (targetName.name === this.name) ? targetName : new $ELM_ELEMENT(GetElement(targetName));
+        const target = (targetName.name === this.name) ?
+          targetName : new $ELM_ELEMENT(GetElement(targetName));
         element.parentNode.insertBefore(target.element, element.nextSibling);
         return this;
       }
       throw new Error(`${targetName} Element does not exist! Function 'insertAfter'`);
     },
+    hide() {
+      this.style({
+        display: 'none',
+      });
+    },
     data(key, value) {
       if (element) {
+        if (!value) return element.dataset[key];
         element.dataset[key] = value;
         return this;
       }
@@ -139,9 +154,9 @@ function $ELM_ELEMENT(element) {
     },
     element,
   };
-};
+}
 
-const CreateElement = (arg) => {
+const CreateElement = (arg, options) => {
   if (arg instanceof HTMLElement) return new $ELM_ELEMENT(arg);
   const arr = arg.split(' ');
   const type = arr.reduce((acc, current) => {
@@ -150,9 +165,14 @@ const CreateElement = (arg) => {
     }
     return acc;
   }, 'div');
-
   const classNames = arr.filter(current => !reservedElements.includes(current)).join();
   const dom = document.createElement(type);
+  if (options) {
+    Object.keys(options).forEach((option) => {
+      const value = options[option];
+      dom[option] = value;
+    });
+  }
   return new $ELM_ELEMENT(dom).css(classNames);
 };
 
@@ -172,9 +192,9 @@ const CreateElementByObject = (type, iterable) => {
 
 export const $ELM = {
   elms: {},
-  create(...args) {
-    if (args.length === 1) return CreateElement(args[0]);
-    return args.map(arg => CreateElement(arg));
+  create(args, options = {}) {
+    if (Array.isArray(args)) return args.map(arg => CreateElement(arg));
+    return CreateElement(args, options);
   },
   build(type, iterable) {
     return CreateElementByObject(type, iterable);
@@ -228,13 +248,13 @@ export const ICACRO = () => {
         .then(response => response.json())
         .catch(err => err);
     },
-    style(styles) {
-      console.log('style is deprecated... use css and require.');
-      const style = $ELM.create('style');
-      style.attr('type', 'text/css');
-      style.append(document.createTextNode(styles));
-      $ELM.get('head').append(style);
-    },
+    // style(styles) {
+    //   console.log('style is deprecated... use css and require.');
+    //   const style = $ELM.create('style');
+    //   style.attr('type', 'text/css');
+    //   style.append(document.createTextNode(styles));
+    //   $ELM.get('head').append(style);
+    // },
     isLoggedIn() {
       return $ELM.get('#hdnIcaState').attr('value').length > 1;
     },
