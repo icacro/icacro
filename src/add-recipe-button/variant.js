86f23d7316eb72164354de81974fbffe3ef58f5d
Michael Larsson <michael.larsson@ica.se>
2017-12-12 09:33:14 +0100

    Move DDL

--- src/add-recipe-button/variant.js
+++ src/add-recipe-button/variant.js
@@ -10,18 +10,29 @@
 'use strict';
 
 import { CROUTIL, ELM } from '../util/main';
+import { insertAfter } from '../util/element-functions';
 
 import './style.css';
 
-// if (hj) hj('trigger','variant6');// eslint-disable-line
+hj('trigger','variant6');// eslint-disable-line
 
 function init() {
   const test = {
-    manipulateDom() {
+    moveButton() {
       const container = ELM.create('add-to-list');
       ELM.get('#ingredients-section h2').insertAfter(container);
       container.append(ELM.get('.coachmark-arrow--left-up'));
       container.append(ELM.get('.js-open-shoppinglist-modal'));
+    },
+    moveDDL() {
+      const ddl = ELM.get('.servings-picker--dynamic');
+      const container = ELM.get('.ingredients__content');
+      container.append(ddl);
+    },
+    manipulateDom() {
+      ELM.push(insertAfter);
+      this.moveDDL();
+      this.moveButton();
     },
   };
   Object.assign(test, CROUTIL());
