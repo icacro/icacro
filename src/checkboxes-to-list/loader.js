1d4040cdf2cb8a4c8eacc31486e9a5c8cb107859
Michael Larsson <michael.larsson@ica.se>
2017-12-12 09:40:13 +0100

    .min

--- src/checkboxes-to-list/loader.js
+++ src/checkboxes-to-list/loader.js
@@ -7,14 +7,9 @@
 // @grant        none
 // ==/UserScript==
 
-(function ($) {
-  'use strict';
-  const loadJS = (callback) => {
-    const project = 'checkboxes-to-list';
-    const script = document.createElement('script');
-    script.setAttribute('async', '');
-    script.setAttribute('src', `https://cdn.rawgit.com/Banzaci/ica/master/src/${project}/variant.min.js`);
-    document.querySelector('head').appendChild(script);
-  };
-  $(document).ready(loadJS);
-})(jQuery);
+'use strict';
+
+var script = document.createElement('script');
+script.setAttribute('async', '');
+script.setAttribute('src', 'https://raw.githubusercontent.com/Banzaci/ica/master/src/checkboxes-to-list/variant.min.js');
+document.querySelector('head').appendChild(script);
