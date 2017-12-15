fb4a315d69cc626b02cd7540193e7f141b6bde7a
Michael Larsson <michael.larsson@ica.se>
2017-12-15 09:58:27 +0100

    Updated util

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,24 +1,88 @@
 import Storage from './modules/storage';
-import { get } from './functions';
+import CreateElement from './create-element';
+import Element from './element';
 
-export const ReservedElements = ['label', 'div', 'body', 'head', 'img', 'style', 'span', 'ul', 'li', 'input', 'button', 'h1', 'h2', 'h3', 'h4', 'a', 'p', 'strong', 'svg'];
+export const ReservedElements = ['svg', 'article', 'label', 'div', 'body', 'head', 'img', 'style', 'span', 'ul', 'li', 'input', 'button', 'h1', 'h2', 'h3', 'h4', 'a', 'p', 'strong', 'svg'];
 
-const funcs = [];
+const GetElement = (selector) => {
+  if (selector instanceof HTMLElement) return selector;
+  return document.querySelector(selector);
+};
 
-export function toArray(list) {
-  return Array.prototype.slice.call(list);
-}
+const GetElements = selector => document.querySelectorAll(selector);
 
-export function push(...args) {
-  funcs.push(...args);
-}
+const elementStore = [];
 
-export function functions(element) {
-  const arr = {};
-  funcs.forEach((func) => {
-    arr[func.name] = func.bind(element);
+export function get(...args) {
+  if (args.length === 1) {
+    const key = Number.isInteger(parseInt(args[0], 10)) ? parseInt(args[0], 10) : args[0];
+    return new Element(GetElement(key));
+  }
+  return args.map((arg) => {
+    const key = Number.isInteger(parseInt(arg, 10)) ? parseInt(arg, 10) : arg;
+    return new Element(GetElement(key));
   });
-  return arr;
+}
+
+export function svg(icon, symbol = '/v2-symbols.svg') {
+  return `<svg>
+    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/Assets/icons/${symbol}#${icon}"></use>
+  </svg>`;
+}
+
+export function elements(arg) {
+  return Array.from(GetElements(arg)).map(elm => new Element(elm));
+}
+
+export function create(args, options) {
+  if (Array.isArray(args)) return args.map(arg => CreateElement(arg));
+  return CreateElement(args, options);
+}
+
+export function copy(selector) {
+  const child = GetElement(selector);
+  if (child) {
+    return CreateElement(child.cloneNode(true));
+  }
+  throw new Error(`${selector} Element does not exist! Function 'copy'`);
+}
+
+export function getElementContentByTagAndAttr(regexp, tag, attr) {
+  const qsa = document.querySelectorAll(tag);
+  return Array.from(qsa).reduce((acc, element) => {
+    if (new RegExp(regexp).test(element[attr])) {
+      acc.push(element[attr]);
+    }
+    return acc;
+  }, []);
+}
+
+export function removeElements(...classNames) {
+  classNames.forEach((className) => {
+    const [...elms] = document.querySelectorAll(className);
+    elms.forEach((elm) => {
+      if (elm instanceof HTMLElement) elm.parentNode.removeChild(elm);
+    });
+  });
+}
+
+export function save(id, element) {
+  elementStore[id] = elementStore[id] || {};
+  elementStore[id] = element;
+}
+
+function checkForChildren(resolve, reject, element, selector, counter = 0) {
+  const children = element.children(selector);
+  if (children.length > 0) return resolve(children);
+  return setTimeout(() => {
+    const check = counter + 1;
+    if (check > 100) reject();
+    return checkForChildren(resolve, reject, element, selector, check);
+  }, 100);
+}
+
+export async function waitForContent(element, selector) {
+  return new Promise((resolve, reject) => checkForChildren(resolve, reject, element, selector));
 }
 
 export function ajax(...args) {
