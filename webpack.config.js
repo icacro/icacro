68b1aa54887f2a8a8b367225f150a3523f95e922
Per Flitig <per.flitig@ica.se>
2017-12-15 16:08:26 +0100

    Lagt till stöd för fler varianter på formen variant.namn.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,33 @@ const LiveReloadPlugin = require('webpack-livereload-plugin');
 const fs = require('fs');
 const webpack = require('webpack');
 
+function getAllVariants(projectName) {
+  return fs.readdirSync(`src/${projectName}`).filter(file => file.startsWith('variant') && !file.includes('min'));
+}
+
+function removeExtension(name) {
+  return name.replace('.js', '');
+}
+
+function getEntries(projectName, fullPath) {
+  const entry = {};
+  getAllVariants(projectName).forEach((name) => {
+    entry[removeExtension(name)] = path.join(path.resolve(__dirname, fullPath), name);
+  });
+  return entry;
+}
+
 module.exports = (env) => {
   try {
     if (!env || !env.project) throw new Error('Missing path "npm run build --env.project ...."');
     const fullPath = `./src/${env.project}`;
     if (!fs.existsSync(fullPath)) throw new Error('No path exist.');
+
     return {
-      entry: path.join(path.resolve(__dirname, fullPath), 'variant.js'),
+      entry: getEntries(env.project, fullPath),
       output: {
         path: path.resolve(__dirname, `src/${env.project}`),
-        filename: 'variant.min.js',
+        filename: '[name].min.js',
       },
       plugins: [
         new webpack.DefinePlugin({
