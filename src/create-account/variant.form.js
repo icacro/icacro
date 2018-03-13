6557f528a7c190a4a18999ee0cd4ee27fd42c78e
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-03-13 15:36:27 +0100

    Create-account loadIframe-funktion

--- src/create-account/variant.form.js
+++ src/create-account/variant.form.js
@@ -25,97 +25,76 @@ const test = {
     container.find('iframe').css('opacity','1');
   },
 
-  iframeStep1() {
-    let headerBarTimeout = window.setTimeout(hideHeaderBarStep1, 10);
-    let hideHeaderBarDeferred = $.Deferred();
-    $.when(hideHeaderBarDeferred).done(function() {
-      test.hideLoader($('.cro-iframe-container'));
-    });
-    function hideHeaderBarStep1() {
-      const iframeInner = $('.cro-iframe-container iframe').contents();
-      if (test.checkHeader(iframeInner,headerBarTimeout,hideHeaderBarDeferred)) {
-        const leadNew = '<p class="lead">Skaffa ICA-kort och få:<br><span class="usp-check"></span>Bonus på dina inköp<br><span class="usp-check"></span>Personliga erbjudanden<br><span class="usp-check"></span>Rabatt på resor och nöjen</p>';
+  iframeDone(iframeType, iframeInner) {
+
+    if(iframeType === 'step1') {
+      //OBS! Bort med länken!!!!
+      const leadNew = '<p class="lead">Skaffa ICA-kort och få:<br><span class="usp-check"></span>Bonus på dina inköp<br><span class="usp-check"></span>Personliga erbjudanden<br><span class="usp-check"></span>Rabatt på resor och <a href="/ansokan/tacksida/">nöjen</a></p>';
+      iframeInner.find('body').addClass('cro-modal cro-step1');
+      iframeInner.find('a.payWithCardLink').attr('target','_blank');
+      iframeInner.find('form').attr('target','step2');
+      iframeInner.find('.step1').prepend(leadNew);
+    } else if(iframeType === 'step2') {
+      const step1=document.getElementById("cro-reg").contentWindow.document;
+      const step2=step1.getElementById("step2").contentWindow.location.href;
+      if (step2 === 'https://www.ica.se/ansokan/?step=6c6f79616c74796e6577637573746f6d6572666f726d') {
         iframeInner.find('body').addClass('cro-modal');
-        iframeInner.find('.step-header').show();
-        iframeInner.find('a.payWithCardLink').attr('target','_blank');
-        iframeInner.find('.payWithCardLinkInfo').hide();
-        iframeInner.find('form').attr('target','step2');
-        //iframeInner.find('form').attr('action','/ansokan/tacksida/');
-
-        iframeInner.find('.step1 p.lead').remove();
-        iframeInner.find('.step1').prepend(leadNew);
-
-        //andra sidor
-        iframeInner.find('ul.choices').addClass('stop');
-        iframeInner.find('#page-wrapper').removeClass('is-skt');
-
-      } else {
-        headerBarTimeout = window.setTimeout(hideHeaderBarStep1, 0);
-      }
-    }
-  },
-
-  iframeStep2() {
-    let headerBarTimeout = window.setTimeout(hideHeaderBarStep2, 10);
-    let hideHeaderBarDeferred = $.Deferred();
-    $.when(hideHeaderBarDeferred).done(function() {
-      setTimeout(function(e) {
-        const step1=document.getElementById("cro-reg").contentWindow.document;
-        const step2=step1.getElementById("step2").contentWindow.location.href;
-        if (step2 === 'https://www.ica.se/ansokan/?step=6c6f79616c74796e6577637573746f6d6572666f726d') {
-          $('.cro-iframe-container iframe').contents().find('#step2').addClass('loaded');
-          test.hideLoader($('.cro-iframe-container iframe').contents().find('.cro-step2-container'));
-          $('.cro-iframe-container iframe').contents().find('html,body').animate({
-            scrollTop: $('.cro-iframe-container iframe').contents().find('#step2').offset().top - 90
-          }, 500);
-        } else if (step2 === 'https://www.ica.se/ansokan/?step=6578697374696e67637573746f6d657261726561') {
-          step1.location = step2;
-        } else {
-          top.location = step2;
-        }
-      },500);
-    });
-    function hideHeaderBarStep2() {
-      const iframeInner=$('.cro-iframe-container iframe').contents().find('#step2').contents();
-      if (test.checkHeader(iframeInner,headerBarTimeout,hideHeaderBarDeferred)) {
-        iframeInner.find('body').addClass('cro-modal');
+        $('.cro-iframe-container iframe').contents().find('.payWithCard, .form-wrapper .form li:first-child label span').html('<a href="//www.ica.se/ansokan/?step=6369766963666f726d" target="cro-reg" class="backToStep1 small"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0.014599280431866646 -0.01454699132591486 31.977949142456055 31.999547958374023" id="edit" width="100%" height="100%"><path d="M31.878 30.051c.215.573.129 1.06-.272 1.461-.315.315-.644.473-1.002.473-.172 0-.358-.029-.53-.1a462.04 462.04 0 0 0-11.328-3.795 2.423 2.423 0 0 1-.63-.401L.501 9.974c-.272-.243-.415-.559-.473-.931a1.537 1.537 0 0 1 .229-1.031c1.017-1.647 2.162-3.065 3.437-4.268C4.897 2.541 6.401 1.381 8.219.25c.73-.415 1.389-.344 1.962.229l17.615 17.715c.2.158.329.372.401.63 2.205 6.717 3.423 10.454 3.68 11.228m-7.489-3.366l2.363-2.363a151.483 151.483 0 0 1-1.461-4.397L8.835 3.271c-.2.158-.501.401-.888.745s-.659.587-.816.716l15.925 15.925c.286.315.43.673.415 1.06a1.497 1.497 0 0 1-.487 1.06c-.315.315-.644.473-1.002.473-.473 0-.845-.143-1.131-.43L4.869 6.795A15.324 15.324 0 0 0 3.365 8.7L19.82 25.226c.286.086.988.301 2.077.644 1.117.344 1.933.616 2.492.816"></path></svg></a>').addClass('backactive');
+        $('.cro-iframe-container iframe').contents().find('#step2').addClass('loaded');
+        test.hideLoader($('.cro-iframe-container iframe').contents().find('.cro-step2-container'));
+        $('.cro-iframe-container iframe').contents().find('html,body').animate({
+          scrollTop: $('.cro-iframe-container iframe').contents().find('#step2').offset().top - 90
+        }, 500);
         iframeInner.find('a').attr('target','_blank').removeClass('modal modal-loaded');
         iframeInner.find('a[href="#terms"]').attr('href','https://www.ica.se/PageFiles/80195/VILLKOR_ICABanken_REKPCX2064_ICA_bonus_formanskund_A4.pdf?epslanguage=sv');
         iframeInner.find('a[href="#create-pultext-modal"]').attr('href','https://www.ica.se/policies/behandling-av-personuppgifter/');
         iframeInner.find('form').attr('target','cro-reg');
-        $('.cro-iframe-container iframe').contents().find('.payWithCard, .form-wrapper .form li:first-child label span').html('<a href="//www.ica.se/ansokan/?step=6369766963666f726d" target="cro-reg" class="backToStep1 small"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0.014599280431866646 -0.01454699132591486 31.977949142456055 31.999547958374023" id="edit" width="100%" height="100%"><path d="M31.878 30.051c.215.573.129 1.06-.272 1.461-.315.315-.644.473-1.002.473-.172 0-.358-.029-.53-.1a462.04 462.04 0 0 0-11.328-3.795 2.423 2.423 0 0 1-.63-.401L.501 9.974c-.272-.243-.415-.559-.473-.931a1.537 1.537 0 0 1 .229-1.031c1.017-1.647 2.162-3.065 3.437-4.268C4.897 2.541 6.401 1.381 8.219.25c.73-.415 1.389-.344 1.962.229l17.615 17.715c.2.158.329.372.401.63 2.205 6.717 3.423 10.454 3.68 11.228m-7.489-3.366l2.363-2.363a151.483 151.483 0 0 1-1.461-4.397L8.835 3.271c-.2.158-.501.401-.888.745s-.659.587-.816.716l15.925 15.925c.286.315.43.673.415 1.06a1.497 1.497 0 0 1-.487 1.06c-.315.315-.644.473-1.002.473-.473 0-.845-.143-1.131-.43L4.869 6.795A15.324 15.324 0 0 0 3.365 8.7L19.82 25.226c.286.086.988.301 2.077.644 1.117.344 1.933.616 2.492.816"></path></svg></a>').addClass('backactive');
+      } else if (step2 === 'https://www.ica.se/ansokan/?step=6578697374696e67637573746f6d657261726561') {
+        step1.location = step2;
       } else {
-        headerBarTimeout = window.setTimeout(hideHeaderBarStep2, 0);
+        top.location = step2;
       }
+    } else if(iframeType === 'other') {
+      //iframeInner.find('ul.choices')...;
+      iframeInner.find('#page-wrapper').removeClass('is-skt');
+      iframeInner.find('a.backButton').remove();
+      iframeInner.find('.step-header').show().html('<h1>Nu är det fixat!</h1>');
     }
+    test.hideLoader($('.cro-iframe-container'));
   },
 
-  iframeOther() {
-    let headerBarTimeout = window.setTimeout(hideHeaderBarOther, 10);
+  loadIframe(iframeType) {
+    let iframeInner = test.getIframeInner(iframeType);
+    let headerBarTimeout = window.setTimeout(hideHeaderBar, 10);
     let hideHeaderBarDeferred = $.Deferred();
     $.when(hideHeaderBarDeferred).done(function() {
-      test.hideLoader($('.cro-iframe-container'));
+      test.iframeDone(iframeType, iframeInner);
     });
-    function hideHeaderBarOther() {
-      const iframeInner = $('.cro-iframe-container iframe').contents();
-      if (test.checkHeader(iframeInner,headerBarTimeout,hideHeaderBarDeferred)) {
-        iframeInner.find('ul.choices').addClass('stop');
-        iframeInner.find('#page-wrapper').removeClass('is-skt');
-        iframeInner.find('a.backButton').remove();
-        iframeInner.find('.step-header').show().html('<h1>Nu är det fixat!</h1>');
-      } else {
-        headerBarTimeout = window.setTimeout(hideHeaderBarOther, 0);
+    function hideHeaderBar() {
+      if (!test.checkHeader(iframeType,headerBarTimeout,hideHeaderBarDeferred)) {
+        headerBarTimeout = window.setTimeout(hideHeaderBar, 0);
       }
     }
   },
 
-  checkHeader(iframeInner,headerBarTimeout,hideHeaderBarDeferred) {
+  checkHeader(iframeType,headerBarTimeout,hideHeaderBarDeferred) {
+    let iframeInner=test.getIframeInner(iframeType);
     const e = iframeInner.find('.easy-signup-header');
     if (e.length) {
       window.clearTimeout(headerBarTimeout);
       hideHeaderBarDeferred.resolve();
       return true;
     }
+  },
+
+  getIframeInner(iframeType) {
+    let iframeInner;
+    if(iframeType === 'step2') {
+      iframeInner = $('.cro-iframe-container iframe').contents().find('#step2').contents();
+    } else {
+      iframeInner = $('.cro-iframe-container iframe').contents();
+    }
+    return iframeInner;
   },
 
   loadModal() {
@@ -126,9 +105,11 @@ const test = {
     iframe.load(function () {
       const iframeInner = iframe.contents();
       if(this.contentWindow.location.href.indexOf('tacksida') !== -1) {
-        test.iframeOther();
+        //load other
+        test.loadIframe('other');
       } else if(this.contentWindow.location.href.indexOf('ansokan') !== -1) {
-        test.iframeStep1();
+        //load step1
+        test.loadIframe('step1');
         const step2container = $('<div class="cro-step2-container pl"><span class="loader"></span><iframe id="step2" name="step2" src="" frameborder="0"></iframe></div>');
         step2container.insertAfter(iframeInner.find('.step1 .form-wrapper'));
       }
@@ -142,12 +123,10 @@ const test = {
       if(iframeInner.find('.icon-checkmark:visible').length) {
         iframeInner.find('.payWithCard, .form-wrapper .form li:last-child').hide();
         test.showLoader(iframeInner.find('.cro-step2-container'));
-        test.iframeStep2();
+        //load step2
+        test.loadIframe('step2');
       }
     });
-    //iframeInner.contents().find('#step2').contents().find('form').on('submit', function(e) {
-    //  //???
-    //});
   },
 
   createModal() {
@@ -161,11 +140,6 @@ const test = {
       test.showLoader($('.cro-iframe-container'));
       test.loadModal();
     }, 50);
-  },
-
-  manipulateDom() {
-    const iframeContainer = ELM.create('div cro-iframe-container');
-    ELM.get('body').append(iframeContainer);
   },
 
 };
@@ -203,18 +177,19 @@ $(document).ready(() => {
     }
 
     let modalApplication = $('.modal-application');
+
     if (modalApplication.length) {
       Object.assign(test, CROUTIL());
-      test.manipulateDom();
+      const iframeContainer = ELM.create('div cro-iframe-container');
+      ELM.get('body').append(iframeContainer);
       modalApplication.click((e) => {
         e.preventDefault();
         test.createModal();
       });
     }
-    //fler länkar?
 
   } else if (window.frameElement.getAttribute('name') === 'cro-reg' || window.frameElement.getAttribute('name') === 'step2') {
-    //hantera omladdning av frame - fixa ev flicker i loader.js
+    //hantera omladdning av iframe - fixa ev flicker i loader.js
     $('body').addClass('cro-modal');
   }
 
