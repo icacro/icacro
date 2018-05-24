047655cfc80ae8f90b433d2168995c06353248f2
Per Flitig <per.flitig@ica.se>
2018-05-24 16:19:23 +0200

    Generate test for improved search on mdsa

---
+++ src/mdsa-autocomplete/variant.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Mdsa-autocomplete
+// @path         //./src/mdsa-autocomplete/variant.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/*
+// @grant        none
+// ==/UserScript==
+
+'use strict';
+
+import { CROUTIL, ELM } from '../util/main';
+// import { triggerHotJar } from '../util/utils';
+import './style.css';
+
+const test = {
+  manipulateDom() {},
+};
+
+$(document).ready(() => {
+  Object.assign(test, CROUTIL());
+  test.manipulateDom();
+  // triggerHotJar('mdsaAutocompleteVariant');
+});
