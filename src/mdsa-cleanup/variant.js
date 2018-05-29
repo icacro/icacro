14f83a655a3f7b6192cfedc2b7cc6677a38c4a94
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-05-29 14:17:18 +0200

    mdsa-cleanup kolumner

--- src/mdsa-cleanup/variant.js
+++ src/mdsa-cleanup/variant.js
@@ -16,19 +16,50 @@ import './style.css';
 const test = {
 
   manipulateDom() {
-    ELM.get('.mdsa-main-grid .right-content').remove();
-    ELM.get('h1 .mini-title').remove();
-    ELM.get('h1 + .truncate').remove();
 
-    //vänta tills loaded
-    ELM.get('fieldset.filtersegment').css('contracted');
+    if (ELM.get('.mdsa-main-grid .right-content').exist()) {
+      ELM.get('.mdsa-main-grid .right-content').remove();
+      ELM.get('.mdsa-main-grid > div').removeClass('xl_size15of20').css('xl_size20of20');
+    }
 
-    const searchContainer = ELM.create('div search-container');
-    searchContainer.append(ELM.get('fieldset.search'));
-    ELM.get('header.page-header').append(searchContainer);
-    ELM.get('.filter-search fieldset.search').remove();
+    if (ELM.get('#content > .grid_fluid > .lg_size5of20').exist()) {
+      ELM.get('#content > .grid_fluid > .lg_size5of20').css('lg_size6of20').removeClass('lg_size5of20');
+    }
+    if (ELM.get('#content > .grid_fluid > .lg_size15of20').exist()) {
+      ELM.get('#content > .grid_fluid > .lg_size15of20').css('lg_size14of20').removeClass('lg_size15of20');
+    }
+    if (ELM.get('#content > .grid_fluid > .xl_size4of20').exist()) {
+      ELM.get('#content > .grid_fluid > .xl_size4of20').css('xl_size5of20').removeClass('xl_size4of20');
+    }
+    if (ELM.get('#content > .grid_fluid > .lg_size16of20').exist()) {
+      ELM.get('#content > .grid_fluid > .xl_size16of20').css('xl_size15of20').removeClass('xl_size16of20');
+    }
 
-    ELM.get('.mdsa-main-grid > div').removeClass('xl_size15of20').css('xl_size20of20');
+    if (ELM.get('fieldset.search').exist()) {
+      const searchContainer = ELM.create('div search-container');
+      searchContainer.append(ELM.get('fieldset.search'));
+      ELM.get('header.page-header').append(searchContainer);
+      ELM.get('.filter-search fieldset.search').remove();
+    }
+
+    const filterSegments = document.querySelectorAll('fieldset.filtersegment');
+    if (filterSegments.length) {
+      let isLoaded = setInterval(checkLoaded, 200);
+      function checkLoaded() {
+        if (document.getElementById('RecipeFilterMenu').classList.contains('loaded')) {
+          clearInterval(isLoaded);
+          for (var i = 0; i < filterSegments.length; i++) {
+            if (!filterSegments[i].classList.contains('selected')) {
+              filterSegments[i].classList.add('contracted');
+            }
+          }
+        }
+      }
+    }
+
+    //Tid ngt som traffic kan få ut? Ngt mer vi behöver?
+    //Design??
+
   },
 
 };
