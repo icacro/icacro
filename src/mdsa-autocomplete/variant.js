d405780f90f00b97bb069fc9619fbcd2e55aa158
Per Flitig <per.flitig@ica.se>
2018-06-07 14:54:28 +0200

    Get all filters and make them searchable

--- src/mdsa-autocomplete/variant.js
+++ src/mdsa-autocomplete/variant.js
@@ -13,12 +13,39 @@ import { CROUTIL, ELM } from '../util/main';
 // import { triggerHotJar } from '../util/utils';
 import './style.css';
 
+const filterStartsWith = q => (
+  f => f.toLowerCase().startsWith(q.toLowerCase())
+);
+
 const test = {
-  manipulateDom() {},
+  filters: [],
+  getFilters() {
+    test.filters = [];
+    $('.filtermenu .filtersegment').each(function () {
+      const type = $('legend', this).text().trim();
+      const list = $.makeArray($('li > a:first-child', this).map((i, e) => $(e).text()));
+      test.filters.push({ type, list });
+    });
+  },
+  findFilter(q) {
+    const hasAtLeastOneFilterStartingWith = s => (t => t.list.some(filterStartsWith(s)));
+    const toFilterTypeWithMatchingFilters = t => (
+      { type: t.type, list: t.list.find(filterStartsWith(q)) }
+    );
+
+    return test.filters.filter(hasAtLeastOneFilterStartingWith(q))
+      .map(toFilterTypeWithMatchingFilters(q));
+  },
+  manipulateDom() {
+    $('.cro .recipe-search').removeClass('sm_gte_hidden');
+  },
 };
 
 $(document).ready(() => {
   Object.assign(test, CROUTIL());
   test.manipulateDom();
+  test.getFilters()
+  console.log(test.filters);
+  console.log(test.findFilter('Bil'));
   // triggerHotJar('mdsaAutocompleteVariant');
 });
