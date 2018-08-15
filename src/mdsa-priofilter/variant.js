a638de23fd3d03d046d28d31469e1530693925f1
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-08-15 17:16:03 +0200

    mdsa-priofilter clickevent

--- src/mdsa-priofilter/variant.js
+++ src/mdsa-priofilter/variant.js
@@ -13,48 +13,56 @@ import { CROUTIL, ELM } from '../util/main';
 import { gaPush } from '../util/utils';
 import './style.css';
 
-const btnToggle = ELM.get('.mob-filter-container .filter-toggle-button');
-let filterlevel;
-if (/barn/.test(window.location.href) || /billiga-veckan/.test(window.location.href) || /under-15-minuter/.test(window.location.href)) {
-  filterlevel=1;
-  if (/#:recipefilter=/.test(window.location.href)) {
-    filterlevel=2;
-  }
-} else {
-  filterlevel=0;
-}
+//Välja bort
+//Filterlevel > 0
 
 const test = {
 
   manipulateDom() {
 
+    let filterlevel;
+    if (/barn/.test(window.location.href) || /billiga-veckan/.test(window.location.href) || /under-15-minuter/.test(window.location.href)) {
+      filterlevel=1;
+      if (/#:recipefilter=/.test(window.location.href)) {
+        filterlevel=2;
+      }
+    } else {
+      filterlevel=0;
+    }
+
+    const btnToggle = ELM.get('.mob-filter-container .filter-toggle-button');
     const btnQuick = ELM.create('a').text('Snabbt');
     const btnCostEff = ELM.create('a').text('Prisvärt');
     const btnChildren = ELM.create('a').text('Barnvänligt');
 
+    const buttonRow = ELM.create('div button-row');
+
+    ELM.get('.mob-filter-container').append(buttonRow);
+
     ELM.get('.mob-filter-container .active-filter-display').remove();
     btnToggle.text('Fler filter');
 
-    test.createButton(btnChildren,'barn');
-    test.createButton(btnCostEff,'billiga-veckan');
-    test.createButton(btnQuick,'under-15-minuter');
+    test.createButton(btnQuick,'under-15-minuter',buttonRow,btnToggle,filterlevel);
+    test.createButton(btnCostEff,'billiga-veckan',buttonRow,btnToggle,filterlevel);
+    test.createButton(btnChildren,'barn',buttonRow,btnToggle,filterlevel);
+    buttonRow.append(btnToggle);
+    ELM.get('#RecipeFilterMenu').insertAfter(buttonRow);
 
   },
 
-  createButton(btn,filter) {
-    btn.css('button').insertAfter(btnToggle);
-    console.log(window.location.href + ' - ' + filter + ' - ' + filterlevel);
-    if (window.location.href.indexOf(filter) === -1) {
-      let useHref;
-      if (filterlevel===0) {
-        useHref = window.location.href + filter + '/';
-      } else if (filterlevel===1) {
-        useHref = window.location.href + '#:recipefilter=' + filter;
-      } else {
-        //
-      }
-      btn.attr('href',useHref);
+  createButton(btn,filter,buttonRow,btnToggle,filterlevel) {
+    buttonRow.append(btn.css('button'));
+    btn.attr('href','#');
+
+    const filtermatch = ELM.get('.filtermenu a[data-urlname=' + filter + ']');
+
+    if(filtermatch.exist()) {
+      btn.css('clickable');
+      btn.click((e) => {
+        filtermatch.click();
+      });
     }
+
   }
 
 };
