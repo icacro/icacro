4a8942d54bc692bfd4bced22615582c2ceaba2cf
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-08-20 15:37:18 +0200

    mdsa-priofilter mutationObserver-fix

--- src/mdsa-priofilter/variant.js
+++ src/mdsa-priofilter/variant.js
@@ -22,10 +22,13 @@ import './style.css';
 
 const filter1='enkel';
 const filter1name='Enkelt';
+const filter1Id='61';
 const filter2='billiga-veckan';
 const filter2name='Prisvärt';
+const filter2Id='138';
 const filter3='under-30-minuter';
 const filter3name='Snabbt';
+const filter3Id='146';
 
 const buttonRow = ELM.create('div button-row');
 
@@ -38,51 +41,58 @@ const test = {
     if (btnToggle.exist()) {
       test.createFilters(btnToggle);
     } else {
-
       const filterWrapper = document.querySelector('.mob-filter-container');
+      let i = 0;
+      setInterval(function() {
+        i++;
+        if (btnToggle.exist()) {
+          console.log('filters ok');
+          test.createFilters(btnToggle);
+        } else if (i === 20) {
+          console.log('filters timeout');
+          clearInterval(this);
+        }
+      }, 500);
+    }
 
-      let btnToggleObserver = new MutationObserver(function(mutations) {
-        for (var i = 0; i < mutations.length; i++) {
-          console.log('checking...');
-          if (btnToggle.exist()) {
-            console.log('found!');
-            test.createFilters(btnToggle);
-            btnToggleObserver.disconnect();
-            break;
+    let filterObserver = new MutationObserver(function(mutations) {
+      let filterId;
+      for (var i = 0; i < mutations.length; i++) {
+        if (mutations[i].target.hasAttribute('data-id')) {
+          filterId = mutations[i].target.getAttribute('data-id');
+          if (filterId === filter1Id || filterId === filter2Id || filterId === filter3Id) {
+            test.filterClasses(mutations[i].target, filterId);
           }
         }
-      });
+      }
+    });
 
-      btnToggleObserver.observe(filterWrapper, {
-        attributes: false,
-        characterData: false,
-        childList: true,
-        subtree: false
-      });
-
-    }
+    filterObserver.observe(document.getElementById('RecipeFilterMenu'), {
+      attributes: true,
+      childList: false,
+      characterData: false,
+      subtree: true
+    });
 
   },
 
   createFilters(btnToggle) {
 
     btnToggle.text('Fler filter');
-
     ELM.get('.mob-filter-container .active-filter-display').css('hidden');
     ELM.get('.mob-filter-container').append(buttonRow);
 
-    test.createButton(filter3name,filter3);
-    test.createButton(filter2name,filter2);
-    test.createButton(filter1name,filter1);
-
+    test.createButton(filter3name,filter3,filter3Id);
+    test.createButton(filter2name,filter2,filter2Id);
+    test.createButton(filter1name,filter1,filter1Id);
     buttonRow.append(btnToggle);
     buttonRow.insertAfter(ELM.get('.mob-filter-container .active-filter-display'));
 
   },
 
-  createButton(btnText,filter) {
+  createButton(btnText,filter,filterId) {
 
-    const btn = ELM.create('a').text(btnText);
+    const btn = ELM.create('a').attr('data-filter-id', filterId).text(btnText);
     const filtermatch = document.querySelector('.filtermenu a[data-urlname=' + filter + ']');
     btn.attr('href','#').css('button').css('btn-' + filter);
     buttonRow.append(btn);
@@ -92,63 +102,41 @@ const test = {
     });
 
     if(filtermatch) {
-      test.createListener(btn,filter,filtermatch);
-      setTimeout(function() {
-        if(filtermatch.getAttribute('data-hits') === '0' || test.hasActiveSiblings(filtermatch)) {
-          btn.css('inactive');
-        } else if (filtermatch.parentNode.classList.contains('active')) {
-          btn.css('active');
-        }
-      },500);
-      return btn;
-    } else {
-      btn.css('inactive').css('no-filter');
-      return btn;
+      test.addClickListener(btn,filter,filtermatch);
     }
+
+    test.filterClasses(filtermatch, filterId);
+    return btn;
 
   },
 
-  hasActiveSiblings(filtermatch) {
-    const activeFilters = filtermatch.parentNode.parentNode.getElementsByClassName('active');
-    for (var i = 0; i < activeFilters.length; i++) {
-      if (activeFilters[i].innerHtml !== filtermatch.innerHtml) {
-        return true;
-      }
-    }
-    return false;
-  },
-
-  createListener(btn,filter,filtermatch) {
-
+  addClickListener(btn,filter,filtermatch) {
     btn.click((e) => {
       if (!btn.hasClass('inactive')) {
-        btn.toggle('active');
         filtermatch.click();
-        setTimeout(function() {
-          if (filter !== filter1) test.checkFilter(filter1);
-          if (filter !== filter2) test.checkFilter(filter2);
-          if (filter !== filter3) test.checkFilter(filter3);
-        },500);
       }
     });
-
   },
 
-  checkFilter(filter) {
+  filterClasses(filtermatch,filterId) {
 
-    const btn = ELM.get('.button-row .btn-' + filter);
-    const filtermatch = document.querySelector('.filtermenu a[data-urlname="'+filter+'"]');
     if (filtermatch) {
-      if (filtermatch.getAttribute('data-hits') === '0' || test.hasActiveSiblings(filtermatch)) {
-        btn.css('inactive');
+
+      const button = ELM.get('.button-row a[data-filter-id="' + filterId + '"]');
+      
+      if(filtermatch.getAttribute('data-hits') === '0') {
+        button.css('inactive');
       } else {
-        btn.removeClass('inactive');
-        if (btn.hasClass('no-filter')) {
-          test.createListener(btn,filter,filtermatch);
-        }
+        button.removeClass('inactive');
       }
+      if(filtermatch.parentNode.classList.contains('active')) {
+        button.css('active');
+      } else {
+        button.removeClass('active');
+      }
+
     } else {
-      btn.css('inactive');
+      button.css('inactive');
     }
 
   }
