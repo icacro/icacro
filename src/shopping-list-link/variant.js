dea9f9c9b669d079d10417d864e7210fb8fbc2fc
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-09-12 10:42:17 +0200

    shopping-list-link close modal fix

--- src/shopping-list-link/variant.js
+++ src/shopping-list-link/variant.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         recipe-toggle-ingredients
-// @path         //./src/mdsa-cleanup/variant.js
+// @path         //./src/shopping-list-link/variant.js
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @match        https://www.ica.se/recept/*
@@ -21,7 +21,8 @@ const test = {
       for (var i = 0; i < mutations.length; i++) {
         if (mutations[i].target.classList.contains('pl-modal__window')) {
           if(mutations[i].target.querySelector('h4').innerHTML === 'Recept lades till i din inköpslista') {
-            const modal = ELM.create('div pl-modal modal-copy');
+
+            const modal = ELM.create('div pl-modal modal-copy').attr('id','cro-overlay');
             const modalInner = ELM.create('div pl-modal__window');
             const closeBtn = ELM.create('a button--icon pl-modal__close-button button--link').css('button').html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="3.092778444290161 4.548774719238281 17.0854434967041 17.08416748046875"><path d="M15.224 13.091l4.582 4.606q.315.291.364.691t-.194.667q-1.042 1.333-2.376 2.376-.267.242-.667.194t-.715-.339l-4.582-4.606-4.606 4.606q-.291.291-.691.339t-.691-.194q-1.309-1.042-2.352-2.352-.242-.291-.194-.691t.339-.691l4.606-4.606-4.582-4.582q-.315-.315-.364-.715t.194-.667q1.018-1.309 2.352-2.376.291-.242.691-.194t.691.364l4.606 4.388 4.582-4.388q.315-.315.715-.364t.667.194q1.333 1.042 2.376 2.376.242.267.194.667t-.364.715z"></path></svg>');
             const toListBtn = ELM.create('a shopping-list-link').css('button').attr('href','/inkopslista/').html('Till inköpslistan');
@@ -37,6 +38,12 @@ const test = {
 
             closeBtn.click((e) => {
               modal.remove();
+            });
+
+            modal.click((e) => {
+              if (e.target.id === 'cro-overlay') {
+                modal.remove();
+              }
             });
 
             break;
