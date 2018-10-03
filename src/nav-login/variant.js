8390461e8a33160bde4faeecc5b9e9a9a1974838
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-10-03 14:37:20 +0200

    nav-login cleanup

--- src/nav-login/variant.js
+++ src/nav-login/variant.js
@@ -8,52 +8,36 @@
 // @grant        none
 // ==/UserScript==
 
-(function($) {
-    'use strict';
+'use strict';
 
-    var test = {
-        addStyles: function () {
-            var styles = '<style type="text/css">' +
-              '#c-login {' +
-              'background: transparent; border: 1px solid #fff; border-radius: 5rem;' +
-              'padding: .2rem 2rem; color:#fff; font-size: 1.4rem;' +
-              'margin: 10px 10px 0 0; height:27px;' +
-              '}' +
-              '#c-login:hover {' +
-              'opacity:0.75; -webkit-transition: fill .1s ease-in-out; transition: fill .1s;' +
-              '}' +
-              '#c-login.black {' +
-              'color:#3F3F40; border-color:#3F3F40;' +
-              '}' +
-              '@media only screen and (max-width: 767px) { #c-login {display:none;} }' +
-              '</style>';
-            $('head').append(styles);
-        },
-        manipulateDom: function () {
-          if(!$('.top-bar-display-name').length) {
-            if ($('.top-bar__wrapper').css('background-color') === 'rgb(230, 0, 100)') {
-              var loginClass='white';
-            } else {
-              var loginClass='black';
-            }
-            var loginBtn = '<a href="#" id="c-login" class="'+ loginClass +'">Logga in</a>';
-            $('.top-bar__right').prepend(loginBtn);
-            $('#c-login').on('click',function(e) {
-              e.preventDefault();
-              if (ga) {
-                ga('send', 'event', 'A/B', 'Klick på loginknapp i nav');
-              }
-              document.querySelector('#js-toggle-avatar .js-track-nav-user-login').click();
-            });
-          }
-        }
-    };
+import { CROUTIL, ELM } from '../util/main';
+import { gaPush } from '../util/utils';
+import './style.css';
 
-    test.addStyles();
+const test = {
 
-    $(document).ready(function (){
-        $('body').addClass('cro');
-        test.manipulateDom();
-    });
+  manipulateDom() {
 
-})(jQuery);
+    if(!$('.top-bar-display-name').length) {
+      if ($('.top-bar__wrapper').css('background-color') === 'rgb(230, 0, 100)') {
+        var loginClass='white';
+      } else {
+        var loginClass='black';
+      }
+      var loginBtn = '<a href="#" id="c-login" class="'+ loginClass +'">Logga in</a>';
+      $('.top-bar__right').prepend(loginBtn);
+      $('#c-login').on('click',function(e) {
+        e.preventDefault();
+        gaPush({ eventAction: 'Klick på loginknapp i nav' });
+        document.querySelector('#js-toggle-avatar .js-track-nav-user-login').click();
+      });
+    }
+
+  }
+
+};
+
+$(document).ready(() => {
+  Object.assign(test, CROUTIL());
+  test.manipulateDom();
+});
