d2ef8615b8a3043c5ec4d1f156a709547ea72e0b
Annika Thylander <annika.thylander@ica.se>
2018-10-09 10:53:07 +0200

    mdsa-close-filter loader.js

--- src/mdsa-close-filter/loader.js
+++ src/mdsa-close-filter/loader.js
@@ -18,7 +18,7 @@ function init() {
   var head = document.querySelector('head');
 
   script.setAttribute('async', '');
-  script.setAttribute('src', 'https://cdn.rawgit.com/Banzaci/ica/vx.x.xx/src/kupong-receptsidan/variant.min.js');
+  script.setAttribute('src', 'https://cdn.rawgit.com/icacro/icacro/v1.0.250/src/mdsa-autocomplete/variant.min.js');
 
   style.appendChild(document.createTextNode(css));
 
