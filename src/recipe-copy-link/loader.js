e94d31dd8c17e13b0e75c6708107f6da3fd84b79
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-10-10 16:13:46 +0200

    recipe-copy-link

---
+++ src/recipe-copy-link/loader.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Banner
+// @path         //./src/recipe-toggle-ingredients/variant.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/*
+// @grant        none
+// ==/UserScript==
+
+/*eslint-disable */
+
+'use strict';
+
+function init() {
+  var script = document.createElement('script');
+  var style = document.createElement('style');
+  var head = document.querySelector('head');
+
+  style.setAttribute('type', 'text/css');
+  style.innerHTML = '.recipe-action-buttons .button--print{visibility:hidden;}';
+
+  script.setAttribute('async', '');
+  script.setAttribute('src', 'https://cdn.rawgit.com/icacro/icacro/v1.0.286/src/recipe-android-share/variant.min.js');
+
+  head.appendChild(script);
+  head.appendChild(style);
+}
+init();
