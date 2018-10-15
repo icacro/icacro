1b7df714cf40b0db6c5ca37192ed4d42f83f4422
Annika Thylander <annika.thylander@ica.se>
2018-10-15 16:02:00 +0200

    mdsa-close-filter fake-orginal

---
+++ src/mdsa-close-filter/variant.orginal.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         testName
+// @path         testPath
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/*
+// @grant        none
+// ==/UserScript==
+
+'use strict';
+
+import { CROUTIL } from '../util/main';
+import { throttle, gaPush } from '../util/utils';
+
+const test = {
+  manipulateDom() {
+    $('.cro .filtersegment div.reset-filter-segment').on("click", function() {
+      gaPush({ eventAction: 'Stäng filter', eventLabel: $(this).closest('legend').text().trim() });
+    });
+  }
+};
+
+
+$(document).ready(() => {
+  Object.assign(test, CROUTIL());
+  test.manipulateDom();
+});
