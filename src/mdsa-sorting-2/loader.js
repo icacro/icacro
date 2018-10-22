ad2ff3b8b3cf57b25192b96ec6d6fc3420db7359
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-10-22 11:28:52 +0200

    mdsa-sorting-2

---
+++ src/mdsa-sorting-2/loader.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Banner
+// @path         //./src/mdsa-sorting/variant.1.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/*
+// @grant        none
+// ==/UserScript==
+
+/*eslint-disable */
+
+'use strict';
+
+function init() {
+  var css = '#recipes .article {opacity:0;} filter-dropdown.filter-dropdown {border:0;} filter-dropdown .toggle-arrow-icon {display:none} .filter-dropdown-selected-content {color:#fff;}';
+  var style = document.createElement('style');
+  var script = document.createElement('script');
+  var head = document.querySelector('head');
+
+  script.setAttribute('async', '');
+  script.setAttribute('src', 'https://cdn.rawgit.com/icacro/icacro/v1.0.228/src/mdsa-sorting/variant.1.min.js');
+
+  style.appendChild(document.createTextNode(css));
+
+  head.appendChild(style);
+  head.appendChild(script);
+}
+init();
---
+++ src/mdsa-sorting-2/variant.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name         mdsa-sorting
+// @path         //./src/mdsa-sorting-2/variant.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/recept/*
+// @grant        none
+// ==/UserScript==
+
+'use strict';
+
+import { CROUTIL, ELM } from '../util/main';
+import { gaPush } from '../util/utils';
+
+const test = {
+
+  manipulateDom() {
+
+    const defaultSorting = 'Saves';
+
+    ELM.get('.filter-dropdown-wrapper filter-option[value="Saves"]').text('Populärast');
+
+    let selectedSorting = getCookie('recipeSortingPreferenceSelected');
+
+    if (selectedSorting === null) {
+      selectedSorting = defaultSorting;
+      document.cookie = 'recipeSortingPreferenceSelected=' + selectedSorting + '; path=/';
+      document.cookie = 'recipeSortingPreference=' + selectedSorting + '; path=/';
+      test.sort(selectedSorting);
+    } else if (selectedSorting === 'Saves') {
+      test.sort(selectedSorting);
+    }
+
+    let listObserver = new MutationObserver(function(mutations) {
+      for (var i = 0; i < mutations.length; i++) {
+        if ((getCookie('recipeSortingPreferenceSelected') !== getCookie('recipeSortingPreference')) && getCookie('recipeSortingPreference') !== null) {
+          document.cookie = 'recipeSortingPreferenceSelected=' + getCookie('recipeSortingPreference') + '; path=/';
+        } else if (getCookie('recipeSortingPreference') === null) {
+          document.cookie = 'recipeSortingPreferenceSelected=Relevance; path=/';
+        }
+      }
+    });
+
+    const config = {
+      attributes: true,
+      childList: true,
+      characterData: false,
+      subtree: true
+    };
+
+    listObserver.observe(document.querySelector('filter-dropdown.filter-dropdown'), config);
+
+  },
+
+  sort(selectedSorting) {
+    ICA.MDSA.recipeList.defaultSort = selectedSorting;
+    ICA.MDSA.recipeList.updateSort();
+    if (selectedSorting === 'Saves') {
+      ELM.get('filter-dropdown').attr('selected','selected');
+      ELM.get('.filter-dropdown-selected .filter-dropdown-selected-content').text('Populärast');
+    }
+  }
+
+};
+
+$(document).ready(() => {
+  if (ELM.get('.filter-dropdown').exist()) {
+    Object.assign(test, CROUTIL());
+    test.manipulateDom();
+  }
+});
