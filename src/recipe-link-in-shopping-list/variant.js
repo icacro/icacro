2e31928bad8853b86b1398f9fe1c92c12ac5c887
Annika Thylander <annika.thylander@ica.se>
2018-11-02 15:00:26 +0100

    recipe-link-in-shopping-list

--- src/recipe-link-in-shopping-list/variant.js
+++ src/recipe-link-in-shopping-list/variant.js
@@ -18,8 +18,50 @@ const test = {
   manipulateDom() {
 
     // klick på lägg till i inköpslista, spara länk till recpet i cookie
+    var btn = document.querySelector(".button.js-open-shoppinglist-modal");
+    btn.addEventListener("click", function(e) {
+      //alert("test");
+      console.log(e);
+
+      setTimeout(function() {
+        document.querySelector(".shoppinglists__item.js-add-to-existing-shoppinglist").addEventListener("click", function(e) {
+          console.log(e);
+        });
+      }, 3000);
+
+
+
+    });
+
+
+    /*<a href="#" class="shoppinglists__item js-add-to-existing-shoppinglist" data-id="6222508" data-secureid="d35961ee-5daa-4bb4-9967-0a680c1322c2">
+                <div class="button button--icon">
+                    <svg>
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/Assets/icons/sprite.svg#plus"></use>
+                    </svg>
+                </div>
+                <div class="shoppinglists__content">
+                    <p class="shoppinglists__item__name">
+                        Att handla 2 november
+                    </p>
+                    <small class="shoppinglists__item__date">2018-11-02</small>
+                </div>
+            </a>*/
 
   },
+  function create_cookie(value) {
+    var cookie = [test.cookieName, '=', JSON.stringify(value), '; domain=.', window.location.host.toString(), '; path=/;'].join('');
+    document.cookie = cookie;
+  },
+  function read_cookie() {
+    var result = document.cookie.match(new RegExp(test.cookieName + '=([^;]+)'));
+    result && (result = JSON.parse(result[1]));
+    return result;
+  },
+  const cookieName = "recipes-in-shopping-list",
+  function save(){
+    var cookie = test.read_cookie();
+  }
 };
 
 $(document).ready(() => {
