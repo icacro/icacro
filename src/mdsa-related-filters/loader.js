27ee72e0806c30508df51ce7e1d44a139a43d092
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-11-15 09:36:52 +0100

    mdsa-related-filters

---
+++ src/mdsa-related-filters/loader.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Banner
+// @path         //./src/mdsa-priofilter/variant.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/*
+// @grant        none
+// ==/UserScript==
+
+/*eslint-disable */
+
+'use strict';
+
+function init() {
+  var css = '@media only screen and (max-width: 699px) {.mob-filter-container .active-filter-display, .filter-toggle-button, #recipe-header {display:none;}}';
+  var style = document.createElement('style');
+  var script = document.createElement('script');
+  var head = document.querySelector('head');
+
+  script.setAttribute('async', '');
+  script.setAttribute('src', 'https://cdn.rawgit.com/icacro/icacro/v1.0.217/src/mdsa-priofilter/variant.min.js');
+
+  style.appendChild(document.createTextNode(css));
+
+  head.appendChild(style);
+  head.appendChild(script);
+}
+init();
---
+++ src/mdsa-related-filters/variant.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         mdsa-priofilter
+// @path         //./src/mdsa-priofilter/variant.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/recept/*
+// @grant        none
+// ==/UserScript==
+
+'use strict';
+
+import { CROUTIL, ELM } from '../util/main';
+import { gaPush } from '../util/utils';
+import './style.css';
+let related;
+
+const test = {
+
+  manipulateDom() {
+
+    if (/^https:\/\/www.ica.se\/recept\/$/.test(window.location.href)) {
+      document.getElementById('page').classList.add('recipe-start');
+    }
+
+    if (document.getElementById('PopularPages')) {
+      related = document.getElementById('PopularPages');
+    } else {
+      related = document.getElementById('RelatedPages');
+    }
+
+    if (related) {
+
+      const relatedFilters = document.createElement('div');
+      document.getElementById('content').querySelector('header').append(relatedFilters);
+
+      relatedFilters.classList.add('related-filters','column-align-center','quicklink-list','pl');
+
+      const relatedList = related.querySelectorAll('.accord-content .grid_fluid a');
+      for (var i = 0; i < relatedList.length; i++) {
+        const relatedUrl = relatedList[i].getAttribute('href');
+        const relatedText = relatedList[i].querySelector('span').innerHTML;
+        const relatedTag = document.createElement('a');
+        relatedTag.innerHTML = relatedText;
+        relatedTag.href = relatedUrl;
+        relatedTag.classList.add('button','button--color-purple','button--purple-light');
+        relatedFilters.append(relatedTag);
+      }
+
+    }
+
+  }
+
+};
+
+$(document).ready(() => {
+  Object.assign(test, CROUTIL());
+  test.manipulateDom();
+});
