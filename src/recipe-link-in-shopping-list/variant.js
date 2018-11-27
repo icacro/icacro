81c1c473cb325acb12bf6094e3afa61e7c89b177
Annika Thylander <annika.thylander@ica.se>
2018-11-27 17:22:42 +0100

    recipe-link-in-shopping-list, display links

--- src/recipe-link-in-shopping-list/variant.js
+++ src/recipe-link-in-shopping-list/variant.js
@@ -17,14 +17,11 @@ const test = {
 
   manipulateDom() {
 
-    var shoppingList = document.querySelector(".ingredient-search");
+    var shoppingList = document.querySelector("#shoppinglist-app");
     if(shoppingList != null) {
-      console.log(shoppingList);
-      shoppingList.appendChild(test.getLinkList());
+      test.displayLinks(shoppingList);
       return;
     }
-    // visa på aktuell inköpslista
-    // när inköpslista välja
 
     var target = document.querySelector(".modal-container.pl");
     var config = { attributes: false, childList: true, subtree: false };
@@ -43,59 +40,40 @@ const test = {
               });
             });
 
-            //var newList = mutation.target.querySelector(".js-activate-add-new-shoppinglist");
             var newList = mutation.target.querySelector(".button.js-add-to-new-shoppinglist");
             if(newList != null) {
               newList.addEventListener("click", function(e) {
                 setTimeout(test.save, 3000);
               });
             }
-
-            //button js-add-to-new-shoppinglist
-
         }
       }
     });
 
     observer.observe(target, config);
+  },
+  displayLinks(shoppingList) {
 
-    //observer.disconnect();
-
-    // klick på lägg till i inköpslista, spara länk till recepet i cookie
-    /*var btn = document.querySelector(".button.js-open-shoppinglist-modal");
-    if (btn == null) {
-      return;
+    var currentList = shoppingList.querySelector(".ingredient-search");
+    if(currentList != null) {
+      currentList.insertAdjacentElement("beforeend", test.getLinkList());
     }
-    btn.addEventListener("click", function(e) {
-      //alert("test");
-      //test.save();
 
-      setTimeout(function() {
-        document.querySelector(".shoppinglists__item").addEventListener("click", function(e) {
-          console.log(e.target.parentNode.value);
-          console.log(e);
-        });
-      }, 3000);
+    var observer = new MutationObserver(function(mutationsList, observer) {
+      for(var mutation of mutationsList) {
+        if (mutation.type == 'childList' && mutation.addedNodes.length > 0) {
+          var list = mutation.target.querySelector(".ingredient-search");
+          if(list != null) {
+            var node = test.getLinkList();
+            if(node) {
+              shoppingList.querySelector(".ingredient-search").insertAdjacentElement("beforeend", node);
+            }
+          }
+        }
+      }
+    });
 
-//document.querySelectorAll(".modal-container.pl")
-
-}); */
-
-
-    /*<a href="#" class="shoppinglists__item js-add-to-existing-shoppinglist" data-id="6222508" data-secureid="d35961ee-5daa-4bb4-9967-0a680c1322c2">
-                <div class="button button--icon">
-                    <svg>
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/Assets/icons/sprite.svg#plus"></use>
-                    </svg>
-                </div>
-                <div class="shoppinglists__content">
-                    <p class="shoppinglists__item__name">
-                        Att handla 2 november
-                    </p>
-                    <small class="shoppinglists__item__date">2018-11-02</small>
-                </div>
-            </a>*/
-
+    observer.observe(shoppingList, { attributes: true, childList: true, subtree: true });
   },
   cookieName: "recipes-in-shopping-list",
   create_cookie(value) {
@@ -132,10 +110,17 @@ const test = {
     if(!recipes) {
       return;
     }
-    var links = document.createElement("span");
+    var listId = window.location.hash.match(/\d+/g);
+    if(listId == null) {
+      return;
+    }
+    var links = document.createElement("div");
+    links.classList.add("shopping-list-recipes");
     links.insertAdjacentHTML("beforeend", "Recept: ");
     recipes.forEach(function(element) {
-      links.insertAdjacentHTML("beforeend", "<a href='" + element.url + "'>" + element.name + "</a>");
+      if(element.list == listId[0]) {
+        links.insertAdjacentHTML("beforeend", "<a class='recipe-link' href='" + element.url + "'>" + element.name + "</a>");
+      }
     });
     return links;
   }
