037e06cc6d39cddfecb56ceef26f9bc856b0cd0c
Åsa Samuelsson <asa.samuelsson@isobar.com>
2018-12-12 10:30:59 +0100

    recipe-video

---
+++ src/recipe-video/variant.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         recipe-toggle-ingredients
+// @path         //./src/mdsa-cleanup/variant.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/recept/*
+// @grant        none
+// ==/UserScript==
+
+'use strict';
+
+import { CROUTIL, ELM } from '../util/main';
+import { ajax, gaPush } from '../util/utils';
+import './style.css';
+
+const test = {
+
+  manipulateDom() {
+    if (document.querySelector('.recipepage')) {
+
+      const video = document.createElement('video');
+      const videos = '<source src="https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm" type="video/webm"><source src="https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4" type="video/mp4">';
+      video.setAttribute('controls','1');
+      video.innerHTML = videos;
+      const mobileVideo = video.cloneNode();
+      mobileVideo.innerHTML = videos;
+
+      const fullscreen = document.querySelector('.hero__image__background');
+      const square = document.querySelector('main .recipe-image-square__image');
+      const mobile = document.querySelector('.hero .recipe-image-square__image');
+
+      if (fullscreen) {
+        test.activateVideo(fullscreen, video);
+      }
+      if (square) {
+        test.activateVideo(square, video);
+      }
+      if (mobile) {
+        test.activateVideo(mobile, mobileVideo);
+      }
+
+    }
+  },
+
+  activateVideo(el,video) {
+    el.append(video);
+    video.addEventListener('play', function(e) {
+      console.log('play');
+      //gaPush({ eventAction: 'Video' });
+    });
+    video.addEventListener('pause', function(e) {
+      console.log('pause');
+      //gaPush({ eventAction: 'Video' });
+    });
+  }
+
+};
+
+$(document).ready(() => {
+  Object.assign(test, CROUTIL());
+  test.manipulateDom();
+});
