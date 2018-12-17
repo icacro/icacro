17e642dd59dc4e76559a6499549e964b8e9b4b25
Annika Thylander <annika.thylander@ica.se>
2018-12-17 17:29:19 +0100

    login, start

---
+++ src/exp068-login/loader.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         loginCoachmark
+// @path         //./src/avatarmenu-offers/variant.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/*
+// @grant        none
+// ==/UserScript==
+
+/*eslint-disable */
+
+'use strict';
+
+function init() {
+  var script = document.createElement('script');
+
+  var head = document.querySelector('head');
+
+  script.setAttribute('async', '');
+  script.setAttribute('src', 'https://cdn.rawgit.com/icacro/icacro/v1.0.XXX/src/exp068-logins/variant.min.js');
+
+  head.appendChild(script);
+}
+
+init();
