6938b8b4a919d44d5d8e98d3a0e64153a12d8086
Annika Thylander <annika.thylander@soprasteria.com>
2019-01-27 22:50:53 +0100

    exp069

---
+++ src/exp069-avatarmenu-offers/loader.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Navigeringsikoner
+// @path         //./src/avatarmenu-offers/variant.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.ica.se/*
+// @grant        none
+// ==/UserScript==
+
+/*eslint-disable */
+
+'use strict';
+
+function init() {
+  var script = document.createElement('script');
+
+  var head = document.querySelector('head');
+
+  script.setAttribute('async', '');
+  script.setAttribute('src', 'https://cdn.rawgit.com/icacro/icacro/v1.0.XXX/src/exp069-avatarmenu-offers/variant.min.js');
+
+  head.appendChild(script);
+}
+
+init();
